d1faf964b70cf4b031d8b6ad43dda3d674b75073
thegoodduck <fkerer40@duck.com>
2024-06-18 14:26:23 -0400

    Changed some things
    Added some new blocked_domains.json
    Updated first_install.py
    Added(Unfunctional) file upload download.
    Added(Functional) Ad stats This is commited to beta because file uploads are not completly working and can crash the brwoser.

--- main.py
+++ main.py
@@ -2,94 +2,17 @@
 # Importing required libraries
 import sys
 import tkinter as tk
-import_sucessfull = True
-try:
-    from PyQt5.QtCore import *
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "--break-system-packages"])
-    import_sucessfull = False
-try:
-    from PyQt5.QtWidgets import *
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "--break-system-packages"])
-    import_sucessfull = False
-
-try:
-    from PyQt5.QtGui import *
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "--break-system-packages"])
-    import_sucessfull = False
-
-try:
-    from PyQt5.QtWebEngineWidgets import *
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQtWebEngine", "--break-system-packages"])
-    import_sucessfull = False
-try:
-    from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "--break-system-packages"])
-    import_sucessfull = False
-
-
-try:
-    from PyQt5.QtNetwork import QNetworkProxy
-except ImportError:
-    import subprocess
-    root = tk.Tk()
-    root.title("Installing Dependencies")
-    label = tk.Label(root, text="Installing Dependencies...")
-    label.pack()
-    root.update()
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "PyQt5", "--break-system-packages"])
-    import_sucessfull = False
-
-import sys
-import requests
-import socket
+import subprocess
 import json
 import time
-if import_sucessfull != True:
-    print("[INFO] Dependencies successfully installed")
-    # Reimport all dependencies
-    from PyQt5.QtCore import *
-    from PyQt5.QtWidgets import *
-    from PyQt5.QtGui import *
-    from PyQt5.QtWebEngineWidgets import *
-    from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
-    from PyQt5.QtNetwork import QNetworkProxy
-    print("[INFO] Reimporting dependencies")
-
+import requests
+import socket
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+from PyQt5.QtNetwork import QNetworkProxy
 
 # Load ad selectors from file
 try:
@@ -98,6 +21,7 @@ try:
 except:
     print("[WARN] Could not load ad selectors")
     ad_selectors = []
+
 class LoadingProgressBar(QProgressBar):
     def __init__(self, *args, **kwargs):
         super(LoadingProgressBar, self).__init__(*args, **kwargs)
@@ -123,6 +47,7 @@ class Adblocker(QWebEngineUrlRequestInterceptor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.load_blocked_domains()
+        self.blocked_ad_count = 0
 
     # Load blocked domains from file
     def load_blocked_domains(self):
@@ -135,8 +60,12 @@ class Adblocker(QWebEngineUrlRequestInterceptor):
         for domain in self.blocked_domains:
             if domain in url:
                 info.block(True)
+                self.blocked_ad_count += 1
                 print(f"[DEBUG] Blocked ad request: {url}")
                 break
+
+    def get_blocked_ad_count(self):
+        return self.blocked_ad_count
 
 # Method to hide ad elements
 def hide_ad_elements(self):
@@ -154,73 +83,7 @@ def hide_ad_elements(self):
     self.tabs.currentWidget().page().runJavaScript(js_function, lambda _: None)
     print("[DEBUG] Ad elements hidden")
 
-
-# Load ad selectors from file
-
-class Adblocker(QWebEngineUrlRequestInterceptor):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.load_blocked_domains()
-
-    # Load blocked domains from file
-    def load_blocked_domains(self):
-        try:
-            with open("blocked_domains.json", "r") as file:
-                self.blocked_domains = json.load(file)
-        except:
-            print("[WARN] Could not load blocked domains")
-            self.blocked_domains = []
-    # Intercept request and block ads
-    def interceptRequest(self, info):
-        url = info.requestUrl().toString()
-        for domain in self.blocked_domains:
-            if domain in url:
-                info.block(True)
-                print(f"[DEBUG] Blocked ad request: {url}")
-                break
-
-# Method to hide ad elements
-def hide_ad_elements(self):
-    js_function = """
-        function hideAdElements(selectors) {
-            selectors.forEach(function(selector) {
-                var elements = document.querySelectorAll(selector);
-                elements.forEach(function(element) {
-                    element.style.display = 'none';
-                });
-            });
-        }
-        hideAdElements(%s);
-    """ % json.dumps(ad_selectors)
-    self.tabs.currentWidget().page().runJavaScript(js_function, lambda _: None)
-    print("[DEBUG] Ad elements hidden")
-
-class LoadingProgressBar(QProgressBar):
-    def __init__(self, *args, **kwargs):
-        super(LoadingProgressBar, self).__init__(*args, **kwargs)
-        self.setStyleSheet("""
-            QProgressBar {
-                border: 2px solid grey;
-                border-radius: 5px;
-                text-align: center;
-            }
-            QProgressBar::chunk {
-                background-color: #1E90FF;
-                width: 10px;
-            }
-        """)
-        self.setMaximum(100)
-        self.setValue(0)
-        self.setFormat("")
-        self.setAlignment(Qt.AlignTop)
-
-    def set_download_speed(self, speed):
-        self.setFormat("")
-
-# Main window
 class MainWindow(QMainWindow):
-
-
     # Constructor
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -318,6 +181,11 @@ class MainWindow(QMainWindow):
         new_tab_btn.triggered.connect(lambda: self.add_new_tab(QUrl('http://www.google.com'), 'New Tab'))
         navtb.addAction(new_tab_btn)
 
+        ad_count_btn = QAction("📊", self)
+        ad_count_btn.setStatusTip('Show blocked ad count')
+        ad_count_btn.triggered.connect(self.show_blocked_ad_count)
+        navtb.addAction(ad_count_btn)
+
         self.add_new_tab(QUrl('http://www.google.com'), 'Homepage')
 
         self.is_privacy_mode = False
@@ -373,145 +241,49 @@ class MainWindow(QMainWindow):
 
         print(f"[DEBUG] Tab changed to index: {i}, URL: {qurl.toString()}")
 
-    # When tab is closed
+    # Close current tab
     def close_current_tab(self, i):
         if self.tabs.count() < 2:
             return
-        browser = self.tabs.widget(i)
-        browser.deleteLater()
         self.tabs.removeTab(i)
-
         print(f"[DEBUG] Tab closed at index: {i}")
 
-    # Method for updating the title
+    # Update window title
     def update_title(self, browser):
         if browser != self.tabs.currentWidget():
             return
         title = self.tabs.currentWidget().page().title()
-        self.setWindowTitle("%s - Dillusion Browser" % title)
+        self.setWindowTitle(f"{title} - Dillusion Browser")
 
-        print(f"[DEBUG] Window title updated: {title}")
-
-    # Action to go to home
+    # Navigate to home page
     def navigate_home(self):
         self.tabs.currentWidget().setUrl(QUrl("http://www.google.com"))
-        print("[DEBUG] Navigating to home")
+        print("[DEBUG] Navigating to homepage")
 
-    # Method for navigate to URL
+    # Navigate to URL
     def navigate_to_url(self):
         q = QUrl(self.urlbar.text())
         if q.scheme() == "":
             q.setScheme("http")
+        self.tabs.currentWidget().setUrl(q)
         print(f"[DEBUG] Navigating to URL: {q.toString()}")
-        self.perform_dns_spoof_check(q)
 
-    # Method to update the URL bar
+    # Update URL bar
     def update_urlbar(self, q, browser=None):
         if browser != self.tabs.currentWidget():
             return
         self.urlbar.setText(q.toString())
         self.urlbar.setCursorPosition(0)
+        print(f"[DEBUG] URL bar updated to: {q.toString()}")
 
-        print(f"[DEBUG] URL bar updated: {q.toString()}")
-
-    # Method to toggle privacy mode
-    def toggle_privacy_mode(self):
-        urls = [self.tabs.widget(i).url() for i in range(self.tabs.count())]
-        if self.is_privacy_mode:
-            self.is_privacy_mode = False
-            self.privacy_btn.setChecked(False)
-            self.reload_tabs_with_profile(QWebEngineProfile.defaultProfile(), urls)
-            print("[DEBUG] Privacy mode disabled")
-        else:
-            privacy_profile = QWebEngineProfile(self)
-            privacy_profile.setHttpCacheType(QWebEngineProfile.MemoryHttpCache)
-            privacy_profile.setPersistentCookiesPolicy(QWebEngineProfile.NoPersistentCookies)
-            self.is_privacy_mode = True
-            self.privacy_btn.setChecked(True)
-            self.reload_tabs_with_profile(privacy_profile, urls)
-            print("[DEBUG] Privacy mode enabled")
-
-    # Method to toggle Tor mode
-    def toggle_tor_mode(self):
-        urls = [self.tabs.widget(i).url() for i in range(self.tabs.count())]
-        if self.is_tor_mode:
-            self.is_tor_mode = False
-            self.tor_btn.setChecked(False)
-            QNetworkProxy.setApplicationProxy(QNetworkProxy())  # Reset to no proxy
-            self.reload_tabs_with_profile(QWebEngineProfile.defaultProfile(), urls)
-            print("[DEBUG] Tor mode disabled")
-        else:
-            self.tor_profile = QWebEngineProfile(self)
-            proxy = QNetworkProxy()
-            proxy.setType(QNetworkProxy.Socks5Proxy)
-            proxy.setHostName("127.0.0.1")
-            proxy.setPort(9050)
-            QNetworkProxy.setApplicationProxy(proxy)
-            self.is_tor_mode = True
-            self.tor_btn.setChecked(True)
-            self.reload_tabs_with_profile(self.tor_profile, urls)
-            print("[DEBUG] Tor mode enabled")
-
-    # Method to clear cookies
+    # Clear cookies
     def clear_cookies(self):
         profile = self.tabs.currentWidget().page().profile()
-        profile.clearHttpCache()
+        profile.clearAllVisitedLinks()
         profile.cookieStore().deleteAllCookies()
         print("[DEBUG] Cookies cleared")
 
-    # Method to reload all tabs with a given profile
-    def reload_tabs_with_profile(self, profile, urls):
-        self.tabs.clear()
-        for url in urls:
-            self.add_new_tab(url, profile=profile)
-        print("[DEBUG] Tabs reloaded with new profile")
-
-    # Method for DNS spoof check
-    def perform_dns_spoof_check(self, qurl):
-        domain = qurl.host()
-        try:
-            public_ip = socket.gethostbyname(domain)
-            local_ip = socket.gethostbyname(domain)  # Use a local DNS resolver
-            if public_ip == local_ip:
-                print(f"[DEBUG] Public and local DNS match for {domain}, fetching directly")
-                self.tabs.currentWidget().setUrl(qurl)
-            else:
-                print(f"[WARN] DNS mismatch for {domain}. Public IP: {public_ip}, Local IP: {local_ip}")
-                self.tabs.currentWidget().setUrl(QUrl(f"http://{public_ip}"))
-        except socket.gaierror as e:
-            print(f"[FATAL] DNS resolution failed for {domain}: {e}")
-            if "chrome://" in qurl.toString():
-                self.tabs.currentWidget().setUrl(qurl)
-            QMessageBox.critical(self, 'DNS Error', f"Failed to resolve DNS for {domain}: {e}")
-            try:
-                self.tabs.currentWidget().setUrl(qurl)
-            except socket.herror as e:
-                QMessageBox.critical(self, 'Fatal', f"{domain} crashed the browser {e}")
-
-    # Method to update the loading progress
-    def update_loading_progress(self, value):
-        self.loading_progress_bar.setValue(value)
-        if value == 100:
-            download_speed = self.get_download_speed()
-            self.loading_progress_bar.set_download_speed("")
-
-    # Method to get the download speed
-    def get_download_speed(self):
-        page = self.tabs.currentWidget().page()
-        start_time = time.time()
-        total_bytes = 0
-
-        def callback(bytes_received):
-            nonlocal total_bytes
-            total_bytes += bytes_received
-
-        page.loadProgress.connect(callback)
-        time.sleep(1)
-        elapsed_time = time.time() - start_time
-        download_speed = (total_bytes / elapsed_time) / (1024 * 1024)
-        return ""
-
-    # Apply custom stylesheet
+    # Apply custom styles
     def apply_styles(self):
         self.setStyleSheet("""
             QMainWindow {
@@ -519,52 +291,92 @@ class MainWindow(QMainWindow):
             }
             QToolBar {
                 background: #FFFFFF;
-                padding: 5px;
-                border: none;
             }
             QTabWidget::pane {
-                border-top: 2px solid #C2C7CB;
-                position: absolute;
-                top: -0.5em;
-                background: #FFFFFF;
+                border: 1px solid lightgray;
             }
-            QTabWidget::tab-bar {
-                alignment: center;
+            QTabBar::tab:selected {
+                background: #1E90FF;
+                color: #FFFFFF;
             }
             QTabBar::tab {
-                background: #E0E0E0;
-                border: 1px solid #C4C4C3;
+                background: #E5E5E5;
                 padding: 10px;
-                border-radius: 5px;
-            }
-            QTabBar::tab:selected, QTabBar::tab:hover {
-                background: #FFFFFF;
+                border: 1px solid lightgray;
             }
             QLineEdit {
-                border: 1px solid #C4C4C3;
-                border-radius: 5px;
                 padding: 5px;
-            }
-            QStatusBar {
-                background: #FFFFFF;
-                padding: 3px;
-            }
-            QToolButton {
-                border: none;
-                background: transparent;
-            }
-            QToolButton:hover {
-                background: #E0E0E0;
+                border: 1px solid lightgray;
                 border-radius: 5px;
+                font-size: 14px;
             }
         """)
-        print("[DEBUG] Styles applied")
+        print("[DEBUG] Custom styles applied")
 
+    # Toggle privacy mode
+    def toggle_privacy_mode(self):
+        self.is_privacy_mode = not self.is_privacy_mode
+        if self.is_privacy_mode:
+            print("[DEBUG] Privacy mode enabled")
+            # Enable privacy features here
+        else:
+            print("[DEBUG] Privacy mode disabled")
+            # Disable privacy features here
 
-# Creating a PyQt5 application
+    # Toggle Tor mode
+    def toggle_tor_mode(self):
+        self.is_tor_mode = not self.is_tor_mode
+        if self.is_tor_mode:
+            print("[DEBUG] Tor mode enabled")
+            self.tor_profile = QWebEngineProfile("tor_profile", self)
+            proxy = QNetworkProxy()
+            proxy.setType(QNetworkProxy.Socks5Proxy)
+            proxy.setHostName("localhost")
+            proxy.setPort(9050)
+            self.tor_profile.setRequestInterceptor(self.adblocker)
+            self.add_new_tab(profile=self.tor_profile)
+        else:
+            print("[DEBUG] Tor mode disabled")
+            self.tor_profile = None
+            self.add_new_tab()
+
+    # Show blocked ad count
+    def show_blocked_ad_count(self):
+        count = self.adblocker.get_blocked_ad_count()
+        QMessageBox.information(self, "Blocked Ads", f"Blocked Ad Requests: {count}")
+        print(f"[DEBUG] Blocked ad count shown: {count}")
+
+    # Update loading progress
+    def update_loading_progress(self, progress):
+        self.loading_progress_bar.setValue(progress)
+        print(f"[DEBUG] Loading progress: {progress}%")
+
+    # Detect if the connection is HTTPS
+    def is_https(self, url):
+        return url.scheme() == 'https'
+
+    # Enable file uploads and downloads
+    def enable_file_uploads_downloads(self):
+        self.page.profile().setHttpAcceptRequestHeaders(['application/octet-stream'])
+        self.page.profile().setHttpAcceptResponseHeaders(['application/octet-stream'])
+        self.page.downloadRequested.connect(self.download_file)
+        self.page.uploadRequested.connect(self.upload_file)
+        print("[DEBUG] File uploads and downloads enabled")
+
+    def download_file(self, download):
+        download.accept()
+        print(f"[DEBUG] Downloading file: {download.url().toString()}")
+
+    def upload_file(self, upload):
+        filename, _ = QFileDialog.getOpenFileName(self, "Select file to upload")
+        if filename:
+            upload.setFile(filename)
+            print(f"[DEBUG] Uploading file: {filename}")
+        else:
+            upload.cancel()
+
+# Main entry point
 app = QApplication(sys.argv)
-app.setApplicationName("Dillusion Browser")
-
-# Creating MainWindow object
-window = MainWindow()
+QApplication.setApplicationName("Dillusion Browser")
+main_window = MainWindow()
 app.exec_()
